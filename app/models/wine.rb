ef69f8fb2bebcf11a60c8702cca7085d786e8721
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-05-02 21:20:00 +0900

    wines_table / enums are created

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,9 @@
 class Wine < ApplicationRecord
-  validates :price, presence: true
+  
 
+  enum taste: {"該当なし": 0, "ライト": 1, "ミディアム": 2, "フル": 3}
+  enum ranking: {"無評価": 0, "最悪": 1, "中下": 2, "まーまー": 3, "美味しい！": 4, "最高！": 5}
   mount_uploader :image, ImageUploader
+
+  validates :price, presence: true
 end
