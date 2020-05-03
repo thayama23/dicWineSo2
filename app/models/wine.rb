35098b241a816dbf911a060a9711380ea5fd34eb
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-05-03 10:20:02 +0900

    rails g devise:install + letter openers are installed

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,7 +2,7 @@ class Wine < ApplicationRecord
   
 
   enum taste: {"該当なし": 0, "ライト": 1, "ミディアム": 2, "フル": 3}
-  enum ranking: {"無評価": 0, "最悪": 1, "中下": 2, "まーまー": 3, "美味しい！": 4, "最高！": 5}
+  enum ranking: {"無評価": 0, "残念": 1, "今一": 2, "まーまー": 3, "美味しい！": 4, "最高！": 5}
   mount_uploader :image, ImageUploader
 
   validates :price, presence: true
