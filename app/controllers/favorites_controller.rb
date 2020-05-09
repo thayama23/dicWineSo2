aa1ff35e824edd3fd6b0e97825b2872a10f16fb1
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-05-09 19:05:42 +0900

    favorite association should be done

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,8 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorite_wines = current_user.favorite_wines
+  end
+  
   def create
     favorite = current_user.favorites.create(wine_id: params[:wine_id])
     redirect_to wines_url, notice: "#{favorite.wine.user.name}さんのブログをお気に入り登録しました"
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
   
 
   if Rails.env.development?
