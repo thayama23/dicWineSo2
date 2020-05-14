db3e570b62fe82279dcd34eb949202d92f94ebb9
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-05-14 17:32:28 +0900

    syste/wine_spec should be done and models/user+wine.spec's should be done

--- spec/models/wine_spec.rb
+++ spec/models/wine_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Wine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it '画像(image)が空白ならバリデーションは通らない。' do
+      @user = FactoryBot.create(:user)
+      @wine = Wine.new(
+       image: nil,
+       price: 7001,
+       kind: "白",
+       country: "日本",
+       origin: "山梨",
+       name: "山梨の白ワイン",
+       vintage: "1980-01-01",
+       ranking: 5,
+       user: @user
+      )
+      expect(@wine).to be_invalid
+    end    
+  end
 end
+
--- spec/system/wine_spec.rb
+++ spec/system/wine_spec.rb
@@ -66,37 +66,8 @@ RSpec.describe 'ワインレビィー機能', type: :system do
       end
     end
 
-    # context 'レビューへラベル（タグ）付機能' do
-    #   it 'ラベル（タグ）付機能の確認' do
-    #     label_create
-    #     user_login
-    #     click_on 'ワインレビューを投稿しよう！'
-        
-    #     attach_file "画像", "./app/assets/images/sample_image.jpeg"
-    #     fill_in "価格", with: 7001
-    #     fill_in "種類", with: "スパークリング"
-    #     fill_in "生産国", with: "USA"
-    #     fill_in "原産地", with: "アリゾナ"
-    #     fill_in "名前", with: "アリゾナのスパークリング"
-    #     binding.irb
-    #     fill_in "収穫年", with: "1970-01-01"
-    #     # select ("1970-01-01" from "収穫年")
-    #     fill_in "評価", with: "最高！"
-
-    #     find(:css, "#wine_label_ids_1[value='1']").set(true)
-    #     find(:css, "#wine_label_ids_1[value='2']").set(true)
-    #     find(:css, "#wine_label_ids_1[value='3']").set(true)
-    #     find(:css, "#wine_label_ids_1[value='4']").set(true)
-    #     find(:css, "#wine_label_ids_1[value='5']").set(true)
-    #     click_on '登録する'
-        
-    #     expect(page).to have_content "個人輸入品 ワイナリー直送 イベント発掘品 小売店品 超お勧め！"
-
-    #   end
-    # end
-
-    context 'レビューへラベル（タグ）付機能' do
-      it 'ラベル（タグ）付機能の確認' do
+    context 'ラベル（タグ）付及び編集機能' do
+      it 'ラベル（タグ）付機能及び編集の確認' do
         label_create
         @user = FactoryBot.create(:user)
         wine = FactoryBot.create(:wine, user: @user)
@@ -105,12 +76,9 @@ RSpec.describe 'ワインレビィー機能', type: :system do
         fill_in 'user[email]', with: 'sample@example.com'
         fill_in 'user[password]', with: 'sample@example.com'
         click_button 'ログイン'
-
-        click_on 'レビュー編集'
+      　click_on 'レビュー編集'
         sleep 1
         page.accept_alert
-
-        # binding.irb
       
         check '個人輸入品'
         check 'ワイナリー直送'
@@ -118,13 +86,7 @@ RSpec.describe 'ワインレビィー機能', type: :system do
         check'小売店品'
         check '超お勧め！'
     
-        # find(:css, "#wine_label_ids_1[value='1']").set(true)
-        # find(:css, "#wine_label_ids_2[value='2']").set(true)
-        # find(:css, "#wine_label_ids_3[value='3']").set(true)
-        # find(:css, "#wine_label_ids_4[value='4']").set(true)
-        # find(:css, "#wine_label_ids_5[value='5']").set(true)
         click_button '更新する'
-        
         expect(page).to have_content "個人輸入品 ワイナリー直送 イベント発掘品 小売店品 超お勧め！"
       end
     end
@@ -150,11 +112,9 @@ RSpec.describe 'ワインレビィー機能', type: :system do
         wine2 = FactoryBot.create(:wine2, user: @user)
         user_login2
         visit wines_path
-        # wine_list = all('.wine_row')
 
         fill_in 'q[kind_cont]', with: '白'
         click_button '検索'
-        # expect(wine_list[0]).to have_content '白テスト'
         #白で検索したから、'赤'は出ない
         expect(page).to have_no_content '赤'
       end
@@ -167,13 +127,12 @@ RSpec.describe 'ワインレビィー機能', type: :system do
         wine2 = FactoryBot.create(:wine2, user: @user)
         user_login2
         visit wines_path
-        # wine_list = all('.wine_row')
-  
-        select('最高！' from:'q[ranking_eq]')
-        binding.irb
+        # select('最高！' from: 'q[ranking_eq]')、では効かない。
+        find("option[value='5']").select_option
+      
         click_button '検索'
-        
-        expect(page).to have_no_content '残念'
+        #'最高！'評価は赤テストだけ、よってそれを拾えれば確認が取れる。
+        expect(page).to have_content '赤テスト'
       end
     end
 
