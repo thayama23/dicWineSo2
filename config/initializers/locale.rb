0375f56f6ecc4a33f1b856a9099139348c5117ab
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-05-23 18:55:57 +0900

    additioanl i18Ns are implemented

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,11 +1,11 @@
 I18n.available_locales = [:en, :ja]
 I18n.default_locale = :ja
 
-require 'i18n'
-I18n.locale # => :en
-I18n.locale = :ja
-I18n.locale # => :ja
-require 'faker'
-Faker::Config.locale # => :ja
-Faker::Config.locale = :en
-Faker::Config.locale # => :en
+# require 'i18n'
+# I18n.locale # => :en
+# I18n.locale = :ja
+# I18n.locale # => :ja
+# require 'faker'
+# Faker::Config.locale # => :ja
+# Faker::Config.locale = :en
+# Faker::Config.locale # => :en
